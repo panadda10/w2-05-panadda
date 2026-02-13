dcf614cc2809658c99799a75147a3a1f9e751bb0
panadda10 <panadda.poonnuch@e-tech.ac.th>
2026-02-13 15:00:38 +0700

    update13/02

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import{StatusBar}from'expo-status-bar';
-import { StyleSheet, Text, View, FlatList,ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Post from './components/Post';
 import postData from './post-data.json'
 export default function App() {
   return (
-    <SafeAreaView style={{ gap: 10 }}>
-<ScrollView>
+    <SafeAreaView>
 <Text  style={{fontSize:50, fontWeight:"500",padding:5,marginLeft:15,marginTop:10}}>Intergram</Text>
 
   <FlatList
@@ -20,10 +19,10 @@ export default function App() {
             profile={item.profile}
           />
         }
-        contentContainerStyle={{ gap: 20 }}
+        contentContainerStyle={{ gap : 20 }}
       />
-</ScrollView>
-    </SafeAreaView>
+
+   </SafeAreaView>
   );
 }
 
